16a90846a7651c8bde37593be2732f4d2a8946d0
Nash Vail <nishantve1@gmail.com>
2019-09-05 11:42:02 +0530

    Added code filter

--- src/pages/filter/codemath.js
+++ src/pages/filter/codemath.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 
 // Constants
@@ -7,12 +8,60 @@ import Constants from "../../utils/constants"
 // Components
 import Header from "../../components/Header"
 import FilterNav from "../../components/FilterNav"
+import ArticlePreview from "../../components/ArticlePreview"
 
 // Styled components
 const Container = styled.main`
   width: ${Constants.BODY_WIDTH["before_first_breakpoint"]};
   margin: 0 auto;
-`;
+`
+const Articles = styled.section`
+  width: 100%;
+  display: grid;
+  /* auto-fit is like you decide the number of columns based on the viewport width*/
+  /* auto-fit is magic */
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  grid-gap: 1em;
+`
+
+function renderArticlePreviews(articles) {
+  // articles is an array of json article objects
+  let articlesArray = []; 
+  articles.forEach(article => 
+    articlesArray.push(<ArticlePreview title={article.node.title} link={article.node.link}/>)
+  );
+  console.log(articles);
+  return articlesArray;
+}
+
+const FilteredArticlesGrid = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query CodeArticlesQuery {
+        allArticlesJson(filter: {
+          category: {
+            eq: "code"
+          }
+        }){
+          edges {
+            node {
+              title
+              link
+              category
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      return (
+        <Articles>{renderArticlePreviews(data.allArticlesJson.edges)}</Articles>
+      )
+    }}
+  />
+)
 
 class CodeMathArticles extends React.Component {
   render() {
@@ -21,7 +70,7 @@ class CodeMathArticles extends React.Component {
         <Header />
         <FilterNav />
         <Container>
-          <h1>Code and math articles</h1>
+          <FilteredArticlesGrid />
         </Container>
       </>
     )
