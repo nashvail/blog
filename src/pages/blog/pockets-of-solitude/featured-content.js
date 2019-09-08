c201e8a8a342bba5f8cccfc17db94eac1d0b08fa
Nash Vail <nishantve1@gmail.com>
2019-09-08 21:15:28 +0530

    Added actual title to featured article

--- src/pages/blog/pockets-of-solitude/featured-content.js
+++ src/pages/blog/pockets-of-solitude/featured-content.js
@@ -1,6 +1,6 @@
 import FeaturedCover from "./assets/featured-cover.png"
 
-export const title = "Title title title"
+export const title = "Pockets of solitude"
 export const link = "/blog/pockets-of-solitude/"
 export const spoiler =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and"
