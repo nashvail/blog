91af78e1de0714d130162a42c7621bf9dc509d2e
Nash Vail <nishantve1@gmail.com>
2019-09-08 10:52:09 +0530

    Added filters gotto figure a way to reuse components lol

--- src/pages/filter/life-philosophy.js
+++ src/pages/filter/life-philosophy.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 
 // Constants
@@ -7,25 +8,73 @@ import Constants from "../../utils/constants"
 // Components
 import Header from "../../components/Header"
 import FilterNav from "../../components/FilterNav"
+import ArticlePreview from "../../components/ArticlePreview"
 
 // Styled components
 const Container = styled.main`
   width: ${Constants.BODY_WIDTH["before_first_breakpoint"]};
   margin: 0 auto;
-`;
+`
+const Articles = styled.section`
+  width: 100%;
+  display: grid;
+  /* auto-fit is like you decide the number of columns based on the viewport width*/
+  /* auto-fit is magic */
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  grid-gap: 1em;
+`
 
-class LifeArticles extends React.Component {
+function renderArticlePreviews(articles) {
+  // articles is an array of json article objects
+  let articlesArray = []; 
+  articles.forEach(article => 
+    articlesArray.push(<ArticlePreview title={article.node.title} link={article.node.link}/>)
+  );
+  console.log(articles);
+  return articlesArray;
+}
+
+const FilteredArticlesGrid = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query PhilArticlesQuery {
+        allArticlesJson(filter: {
+          category: {
+            eq: "philosophy"
+          }
+        }){
+          edges {
+            node {
+              title
+              link
+              category
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      return (
+        <Articles>{renderArticlePreviews(data.allArticlesJson.edges)}</Articles>
+      )
+    }}
+  />
+)
+
+class PhilosophyArticles extends React.Component {
   render() {
     return (
       <>
         <Header />
         <FilterNav />
         <Container>
-          <h1>Life and philosophy articles</h1>
+          <FilteredArticlesGrid />
         </Container>
       </>
     )
   }
 }
 
-export default LifeArticles
+export default PhilosophyArticles
