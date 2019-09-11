3017110324072b214e90b17b4b48c58858f7e907
Nash Vail <nishantve1@gmail.com>
2019-09-11 16:33:48 +0530

    Imported more medium articles

--- src/pages/filter/codemath.js
+++ src/pages/filter/codemath.js
@@ -17,6 +17,17 @@ import { renderArticlePreviews } from "../../utils/helpers";
 const Container = styled.main`
   width: ${Constants.BODY_WIDTH["before_first_breakpoint"]};
   margin: 0 auto;
+  @media screen and (max-width: ${Constants.BREAKPOINTS[0]}) {
+    max-width: ${Constants.BODY_WIDTH["after_first_breakpoint"]};
+  }
+
+  @media screen and (max-width: ${Constants.BREAKPOINTS[1]}) {
+    max-width: ${Constants.BODY_WIDTH["after_second_breakpoint"]};
+  }
+
+  @media screen and (max-width: ${Constants.BREAKPOINTS[2]}) {
+    width: 90%;
+  }
 `
 
 const FilteredArticlesGrid = ({ children }) => (
