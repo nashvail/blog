d16a0ffa79fa4343e7c1fb224cfbe7477cd9f6f3
Nash Vail <nishantve1@gmail.com>
2019-10-09 13:08:04 +0530

    Initial in place responsiveness still sucks

--- src/pages/blog/pockets-of-solitude/index.js
+++ src/pages/blog/pockets-of-solitude/index.js
@@ -2,17 +2,20 @@ import React from "react"
 import styled from "styled-components"
 
 // Constants
-import Constants from "../../../utils/Constants";
+import Constants from "../../../utils/Constants"
 
 // Assets
 import "../../../utils/global.css"
 import s_ from "./styles.module.css"
 import heroImage from "./assets/bubble.png"
-import connectedImage from "./assets/connected.png"
 
 // Components
 import Header from "../../../components/Header"
-import Footer from "./../../../components/Footer";
+import Footer from "./../../../components/Footer"
+import SideCard from "../../../components/SideCard"
+import RelatedMiscArticles from "../../../components/RelatedArticles/RelatedMiscArticles";
+
+import Content from "./content"
 
 // Styled components
 const Container = styled.main`
@@ -59,8 +62,30 @@ const HeroContainer = styled.section`
     flex-direction: column-reverse;
     margin-top: 2rem;
   }
-`;
+`
 
+const Inner = styled.main`
+  width: ${Constants.BODY_WIDTH["before_first_breakpoint"]};
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 72ch 1fr;
+  grid-gap: 3rem;
+
+  @media screen and (max-width: ${Constants.BREAKPOINTS[0]}) {
+    grid-template-columns: auto;
+    grid-gap: 2rem;
+    width: 70%;
+    max-width: 75ch;
+  }
+
+  @media screen and (max-width: ${Constants.BREAKPOINTS[1]}) {
+    width: 75%;
+  }
+
+  @media screen and (max-width: ${Constants.BREAKPOINTS[2]}) {
+    width: 85%;
+  }
+`
 
 const Hero = () => (
   <HeroContainer>
@@ -75,7 +100,6 @@ const Hero = () => (
   </HeroContainer>
 )
 
-
 class PocketsOfSolitude extends React.Component {
   render() {
     return (
@@ -83,218 +107,18 @@ class PocketsOfSolitude extends React.Component {
         <Header />
         <Container>
           <Hero />
-          <Article>
-            <div className={s_.shaper}></div>
-            <h3>Unpublished Draft----------</h3>
-            <p>
-              <span className={s_.firstcharacter}>I</span> love early mornings. The
-              loud mating calls of the birds, the smoke from yesterday, all
-              settled on the tar and the cold fresh air that I can pull in
-              through my nostrils and into my lungs. But really, the best part
-              about mornings is fewer people. I love hitting the gym in morning
-              with so few people, for one, I don't have to wait for my turn on
-              the treadmill or the fact that if I am late than 7 AM someone
-              always takes away the 5 pound dumbbells. And because mornings are
-              so precious to me I have taught myself to fall asleep early. It
-              required baking in certain habits in the last few hours that lead
-              to sleep. For example, no junk food, or coffee, or watching or
-              reading something that sends my mind into a whirlwind of thoughts.
-              Basically, saying no to anything that can potentially induce
-              insomnia.
-            </p>
-            <p>
-              That one night though, it was different. I found myself watching
-              the movie "Her", way past my bedtime. Her is one of those Sci-Fi
-              movies where technology has progressed to a point where computers
-              have started to sound real and even behave real, so real that you
-              can fall in love with them. Like, literally, love. Which as much
-              as I know is sharing details of your day over text and calls, but
-              anyway. So this person, Theodore, in the movie falls in love with
-              his computer, which I thought would be weird when it's eventually
-              confessed to a friend around halfway through the movie, but
-              instead, all his friend asked was ... "Is it (Theodore's
-              relationship with his computer) real?" and even though I hoped for
-              a conclusion, the whole scene ended with an "I don't know" from
-              both sides. And I was stuck.
-            </p>
-            <p>
-              My brain--Isn't it just great to blame your brain for the shit you
-              do--picked this up and started questioning everything digital,
-              everything that doesn't exist outside of a screen, and then, it
-              was 2 AM. My plans of waking up at 5:30 was obliterated of course,
-              along with it died the pride I take in my work.
-            </p>
-            <p>
-              A good part of the population and yes, me and even you spend hours
-              creating things that only exist as flickering lights on a thin
-              glass slab. I am talking about apps and games and all the other
-              things that we use on our little phones and tablets and laptops.
-              The smartest minds today come together each morning and
-              essentially discuss how the pixels on the screens of their users
-              are going to glow, and how they can hook the users to keep looking
-              at those meaningless flickering lights. What these people build,
-              or you build and I build is digital, with no tangible real
-              existence and when I think of things this way, everything seems a
-              little synthetic. It’s as if I go back to a time before technology
-              and show the people of past a piece of software on an iPad that my
-              team built, and these people who are seeing either sow seeds or
-              build real things, like buildings and vases, castles and pyramids.
-              I am afraid that one of them is going to look into the screen and
-              question, “is it real?” And I am afraid I won’t have an instant
-              answer to puke out and our conversation will quickly turn into an
-              argument of me insisting YES, mostly just to save my pride.
-            </p>
-            <p>
-              I did let the question of, is the work we do real stay with me and
-              even spread the pollens of it over almost everyone I met in the
-              weeks following that night. Here's the most common answer and
-              probably the only one I got, "Yes, the work we do is real because
-              it has real world implications". Correct. I don't disagree, except
-              just a small part of me which likes print design and seeing things
-              go from screen to something that I can touch and hold and wear,
-              that part still finds the digital work synthetic. But it doesn't
-              matter, because it's true, reality today *is* shaped by the
-              glowing pixels. It's a crazy thought if you ponder, that something
-              can not exist in reality and still shape it. That's the power of
-              screens.
-            </p>
-            <p>
-              It's not that the screens are controlling autonomous evil beings,
-              it's still us, the humans, congregated in corporations controlling
-              rest of the population through screens. And because screens are so
-              ubiqutous the power to control and brainwash has been grossly
-              magnified in the past few decades. I bet from where you're
-              sitting, if you get up and walk to the door closest to you, on the
-              way you'll encounter more than 3 screens either kept on tables or
-              held in hands, or raised slightly above eye level with the front
-              camera on. Hell I can even make it a 10 or 20 and it'll be true. I
-              mean even you’re looking at a screen right now [look what I am
-              doing], and I am channeling my thoughts through it into your
-              heads, thoughts that can potentially change the way you look at
-              the world, at least for a week or even a lifetime, who knows. I
-              couldnt have done this before screens. There were just two ways,
-              publish what I have to say in print, which is a hell lot hard than
-              publishing an unedited pile of thoughts I had at 2AM on my
-              website. Or, stand at a busy intersection and shout and expect 4
-              people to hear it and even one of them to come and ask me to shut
-              up. You can't do that now, you can't kick me off my shouting
-              pedestal. And it's just not me, it's everybody. Channeling and
-              consuming and shaping through twitter and facebook and instagram.
-              It's too much.
-            </p>
-            <figure>
-              <img src={connectedImage} />
-            </figure>
-            <p>
-              The human brain is not designed to absorb such amounts of bullshit
-              24/7. The constant overload of mostly useless information, like
-              where Becky is having her vacation or the pictures of Jonthan’s
-              kid or the fact that someone went paragliding for the first time,
-              all of this quickly turns your brain into a mush and you into a
-              lab rat constantly chasing and clicking this tapping that
-              scrolling there. The biggest part of the problem is that you
-              cannot do much about it. There are people far smarter than you,
-              with PhDs and in labcoats building all of this to constantly keep
-              you hooked. They know your dopamine hit points, everything is
-              engineered to keep you addicted, turn you into a junkie for
-              notifications, and if you don't stop, pause and think you're going
-              to drive yourself insane. You're already halfway there, if a
-              screen is the first thing you look at in the morning and the last
-              thing at night, we really need to talk.
-            </p>
-            <p>
-              know how it is. How it feels almost impossible to resist looking
-              at the phone the second you wake up. The sound of 10 successive
-              notifications at once has started to feel better than the sex you
-              had last night. People don’t want to get bored anymore, it’s as if
-              boredom bites them, scratches the insides of their skulls, puts a
-              saliva soaked fingertip inside their ears, wets their socks,
-              nibbles on their earlobes or licks the side of their faces.
-            </p>
-            <p>
-              An average person unlocks their phone 150 times a day, most of it
-              is for no reason, it’s automatic, facebook sewed in our DNAs to
-              make us want to check the phone over and over again. It’s your
-              struggle against yourself. It’s why we look up to people who go on
-              a tech detox for 10 days, as if going on tech detox is an
-              achievement, humans were on tech detox for 5 fucking million years
-              you idiot, it’s tech, it’s not breathing/oxygen. To keep your
-              sanity safe you will have to create boundaries. I have talked at
-              length about keeping distractions quarantined while at work here,
-              but today I’ll talk about what you can do outside work, your daily
-              life, how you can find a bubble for yourself that’ll shield you
-              from all the noise. How in the constant chaos you can find for
-              yourself small pockets of solitude.
-            </p>
-            {/* <h2>There is a party in my head and no one's invited</h2> */}
-            <p>
-              <span className={s_.firstcharacter}>I</span>t is believed that forming a habit is easier if you anchor it to
-              something existing, like meditating after you've taken a shower or
-              reading a book before going to sleep e.t.c e.t.c e.t.c. A while
-              ago I decided I'll check my phone in the morning only after I have
-              brushed my teeth. So since then, from the moment I wake up, drink
-              water, take a shit, brush my teeth, gag on my tongue cleaner to
-              when I wash and wipe my face, I am offline. The first 30 minutes
-              of my day is for me. It's not easy, you realise how tough it is to
-              sit on the crapper, alone, with just your thoughts. You feel me?
-              Yes? That's exactly why you need to follow this. It's absurd that
-              executing something this simple is so excruciating. Let's see how
-              many days you can go without checking your phone till you have
-              brushed your teeth. And just so we're clear, don't start brushing
-              your teeth the moment you wake up just so you can check your phone
-              sooner, that's .. stupid. Of course I didn't do that and neither
-              should you.
-            </p>
-            <p>
-              Epicurus, an old time greek philosopher said, “To eat and drink
-              without a friend is to devour like the lion and the wolf.”. Which
-              is to eat like animals, eluding to the fact that humans are social
-              creatures and it's kinda odd to sit and eat alone. And that being
-              a social animal part of our being that has been carried from
-              hunting and living in small groups is the reason for the stigma
-              against doing things alone. It feels uncomfortable both to the
-              person doing it and to the ones watching it. Of course you're up
-              for it never miss a chance of just hanging out but it's not the
-              cave times anymore. We're a subject to over-socialisaiton which
-              might be a good thing if it was online, but right now it's through
-              screens. We're as a species aren't designed to socialise through
-              screens and even though you can have an hour long chat with your
-              close buddies on a whatsapp group, it'll never be as satisfying as
-              sitting around table making fun of Jeff. Poor Jeff, gets owned at
-              every gathering. On top of that socialisation through the screens
-              can have diminishing effects, according to this 1995 study. So
-              Epicurus was correct for his time. If I have a choice I choose
-              having my meals alone, not alone watching Netflix just me and my
-              plate of food. For however long it takes to eat the entire thing I
-              can recollect my thoughts and be with myself. If that's not
-              possible I'd recommend you have an evening snack, preferably
-              healthy, alone and offline.
-            </p>
-            <p>
-              For the rest of the day, a good part of which is spent working I
-              try my best to stick with what I discuss in this article of mine
-              here. TL;DR work in blocks and for the blocks have your
-              notifications turned off and social and other time chewing sites
-              blocked.
-            </p>
-            <p>
-              If there's one thing you take away from this article, it's this,
-              allow yourself to be bored. If you're waiting in a queue watch out
-              don't just leap for your phone, wait, think. And if nothing works
-              just force yourself to lie down and stare at the fucking ceiling
-              for 30 minutes, realise your brain is buzzing with all the
-              scrolling and tapping all day long. If you don't feel the nausea,
-              you're good, maybe we're evolving towards being sane in the face
-              of mind numbing information flooding. Which is a good thing. This
-              article was born out of my personal frustration from watching
-              people around me constantly picking up and staring at their
-              phones, erasing their fingeprints against the glass. Even as I was
-              closing this article, I looked up from my laptop, I noticed the
-              phone on the desk in-front of me buzzed, it was my coworker’s, he
-              picked it up, unlocked it and as the blue light from it flooded
-              his tired eyes, he smiled.
-            </p>
-          </Article>
+          <Inner>
+            <Article>
+              <Content />
+            </Article>
+            <aside>
+            <RelatedMiscArticles/>
+            <SideCard style={{
+              height: "200px",
+              marginTop: "5rem"
+            }}/>
+            </aside>
+          </Inner>
           <Footer />
         </Container>
       </div>
