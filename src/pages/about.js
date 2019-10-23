d49bc3cfc76313fe9ae8779fdd03e70c8bfc003c
Akshay <aks28id@gmail.com>
2019-10-23 16:46:28 +0530

    file import issues resolved

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 
 // Custom components
-import Header from '../components/header';
+import Header from '../components/Header';
 
 export default () => (
   <div>
