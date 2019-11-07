a7d5cffc39e8f26178b5110916b238fa0724e1f8
Nash Vail <nishantve1@gmail.com>
2019-11-07 12:49:42 +0530

    Minor updates to the about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -6,13 +6,15 @@ import Constants from "../../utils/Constants"
 
 // Custom components
 import Header from "../../components/Header"
-import SEO from "../../components/SEO";
+import SEO from "../../components/SEO"
 
 // Import styles
 import s_ from "./styles.module.css"
 
-const Main = styled.main`
-  margin: 15rem auto 0 auto;
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 4fr 2fr;
+  margin: 2rem auto 0 auto;
   width: ${Constants.BODY_WIDTH["before_first_breakpoint"]};
   position: relative;
 
@@ -29,40 +31,51 @@ const Main = styled.main`
   }
 `
 
-const Content = styled.article`
-  width: 75%;
-  padding: calc(60px + 1.5rem);
+const Main = styled.main`
 `
+
+const Content = styled.article`
+  width: 100%;
+  padding: calc(60px + 1.5rem);
+  padding-left: calc(60px + 1rem);
+`
+
+const Side = styled.aside``
 
 export default () => (
   <>
-    <SEO title="About Nash"/>
+    <SEO title="About Nash" />
     <Header />
-    <Main>
-      <div className={s_.nash}>
-        <span>NASH</span>
-        <b></b>
-        <span>VAIL</span>
-      </div>
-      <Content>
-        <p>
-          Nash is a Front end Developer, illustrator, graphic and UI designer
-          and not a human. Nash is a Front end Developer, illustrator, graphic
-          and UI designer and not a human. Nash is a Front end Developer,
-          illustrator, graphic and UI designer and not a human.
-        </p>
-        <p>
-          Nash is a Front end Developer, illustrator, graphic and UI designer
-          and not a human. Nash is a Front end Developer, illustrator, graphic
-          and UI designer and not a human. Nash is a Front end Developer,
-          illustrator, graphic and UI designer and not a human.
-        </p>
-        <h2>Reading list</h2>
-        <p>Sample text for the reading list section of the page</p>
+    <Container>
+      <Main>
+        <div className={s_.nash}>
+          <span>NASH</span>
+          <b></b>
+          <span>VAIL</span>
+        </div>
+        <Content>
+          <p>
+            Nash is a Front end Developer, illustrator, graphic and UI designer
+            and not a human. Nash is a Front end Developer, illustrator, graphic
+            and UI designer and not a human. Nash is a Front end Developer,
+            illustrator, graphic and UI designer and not a human.
+          </p>
+          <p>
+            Nash is a Front end Developer, illustrator, graphic and UI designer
+            and not a human. Nash is a Front end Developer, illustrator, graphic
+            and UI designer and not a human. Nash is a Front end Developer,
+            illustrator, graphic and UI designer and not a human.
+          </p>
+          <h2>Reading list</h2>
+          <p>Sample text for the reading list section of the page</p>
 
-        <h2>Learning list</h2>
-        <p>Sample test for the learning list section of the page.</p>
-      </Content>
-    </Main>
+          <h2>Learning list</h2>
+          <p>Sample test for the learning list section of the page.</p>
+        </Content>
+      </Main>
+      <Side>
+        <p>Social media and contact links</p>
+      </Side>
+    </Container>
   </>
 )
