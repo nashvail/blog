a8e2e9ad4ac3a508d9232b80a55346948938e740
Nash Vail <nishantve1@gmail.com>
2020-03-31 14:37:15 +0530

    Restarted creativity article

---
+++ src/pages/blog/creativity-1/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components"
+
+function Paradoxes () {
+  return (
+    <h1>Hello this is article, this work you?</h1>
+  );
+}
+
+export default Paradoxes;
